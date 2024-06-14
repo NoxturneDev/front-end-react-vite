d8094210ef5d25b10b3e9882e41a8c2fb5724f47
Fahmi Zain <fahmizain000@gmail.com>
2024-06-15 06:27:21 +0700

    fix: finxing navbar layout and adding notification feature

--- src/components/pages/Sidebar.jsx
+++ src/components/pages/Sidebar.jsx
@@ -1,16 +1,49 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaBell } from "react-icons/fa";
+
+import * as React from "react";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { Dialog } from "@radix-ui/react-dialog";
 
 function Sidebar() {
   return (
     <section className="shadow-lg w-80 min-h-screen py-3 flex flex-col justify-between">
       <div>
         <div className="flex items-center justify-around">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <h1 className="font-semibold text-xl">Aplikasi Keuangan</h1>
+          <h1 className="font-semibold text-xl">Daftar Pelanggan</h1>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button>
+                <FaBell />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56 bg-white shadow-md">
+              <DropdownMenuLabel>Pelanggan Baru</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button className="shadow-none">+62 81981389131</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent className="bg-white rounded-md shadow-sm">
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Apakah anda ingin memula percakapan dengan +62 81981389131</AlertDialogTitle>
+                      <AlertDialogDescription>This action cannot be undone. This will permanently delete your account and remove your data from our servers.</AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction className="bg-blue-500 text-white hover:text-black border-blue-500 border hover:border-black">Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         <p className="px-7 mt-3 text-base">Lorem ipsum dolor sit amet consectetur adipisicing</p>
         <nav className="mt-4 px-7">
