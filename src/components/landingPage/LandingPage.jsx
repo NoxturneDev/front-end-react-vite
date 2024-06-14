141c0101338ded783dad0dd0530ec9713559cfc2
Fahmi Zain <fahmizain000@gmail.com>
2024-06-14 12:51:58 +0700

    feat: create landing page (unfinished)

---
+++ src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../pages/Navbar"
+import Hero from "../pages/Hero"
+
+function LandingPage() {
+  return (
+    <>
+     <Navbar /> 
+     <Hero />
+    </>
+  )
+}
+
+export default LandingPage
