717e62f09d2b5059fc52996b87b02c85bb536bd6
Fahmi Zain <fahmizain000@gmail.com>
2024-06-14 18:43:31 +0700

    fix: fixing dashboard layout

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,18 +1,12 @@
 import Sidebar from "../pages/Sidebar";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import ChatPage from "../pages/ChatPage";
 
 function Dashboard() {
   return (
     <section className="flex">
       <Sidebar />
-      <div className="flex flex-col justify-between p-5 w-full">
-        <h1 className="text-2xl font-semibold">Dashboard</h1>
-        <div className="flex gap-x-5">
-          <Input className="w-full" type="email" placeholder="Email" />
-          <Button className="bg-blue-500 rounded text-white">Submit</Button>
-        </div>
-      </div>
+      <ChatPage title={"+62 818091816"} />
+      <ChatPage title={"Helper AI"} />
     </section>
   );
 }
