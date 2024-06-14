861eef7f3c297aa16edb46c503d4621d91a4691b
Fahmi Zain <fahmizain000@gmail.com>
2024-06-14 18:42:28 +0700

    feat: creating component for bubble chat

---
+++ src/components/pages/ChatPage.jsx
@@ -0,0 +1,45 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import BubbleChat from "./BubbleChat";
+import { useState } from "react";
+
+function ChatPage({ title }) {
+  const [chat, setChat] = useState("");
+  const [bubbleChat, setBubbleChat] = useState([]);
+
+  const handleAddChat = () => {
+    const newChat = {
+      id: bubbleChat.length + 1,
+      chat: chat,
+    };
+
+    setBubbleChat((prev) => [...prev, newChat]);
+    setChat("");
+    console.log(test);
+  };
+
+  const handleChange = (e) => {
+    setChat(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col border-r justify-between p-5 w-1/2 h-screen">
+      <div>
+        <div className="pb-5 border-b mb-5">
+          <h1 className="text-2xl font-semibold">{title}</h1>
+        </div>
+        {bubbleChat.map((item) => (
+          <BubbleChat key={item.id} chat={item.chat} variant="reciver" />
+        ))}
+      </div>
+      <div className="flex gap-x-5">
+        <Input value={chat} onChange={handleChange} className="w-full" type="email" placeholder="Email" />
+        <Button onClick={handleAddChat} className="bg-blue-500 rounded text-white">
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ChatPage;
